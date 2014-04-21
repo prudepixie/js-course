37778d92146a16fbad3a04709dadb48647307d2e
Wendy Hsu <wndyhsu@gmail.com>
2014-04-21 14:47:02 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-alert('hi');
+
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = Robin.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
